1e49591a1141f7b312c6b2a2ef75f3d79d03c9fd
Piotr Przetacznik <piotr.przetacznik@gmail.com>
2015-07-14 00:18:03 +0200

    Add assembling of requests on shell socket

--- lib/ielixir/shell.ex
+++ lib/ielixir/shell.ex
@@ -8,20 +8,38 @@ defmodule IElixir.Shell do
 
   def init(opts) do
     sock = IElixir.Utils.make_socket(opts, "shell", :router)
-    { :ok, { sock, sock } }
+    { :ok, { sock, [] } }
   end
 
   def terminate(_reason, { sock, _ }) do
     :erlzmq.close(sock)
   end
 
-  def handle_info({ :zmq, _, data, [] }, state = { sock, _id }) do
-    Logger.info("Shell message received")
-    { :noreply, state }
+  def handle_info({ :zmq, _, msg, flags }, state = { sock, message_buffer }) do
+    case assemble_message(msg, flags, message_buffer) do
+      { :buffer, buffer } ->
+        { :noreply, { sock, buffer } }
+      { :msg, message } ->
+        process(message, sock)
+        { :noreply, { sock, [] } }
+    end
   end
   def handle_info(msg, state) do
     Logger.warn("Got unexpected message on shell process: #{inspect msg}")
     { :noreply, state}
+  end
+
+  defp assemble_message(message, flags, message_buffer) do
+    message_buffer = [message | message_buffer ]
+    if :rcvmore in flags do
+      { :buffer, message_buffer }
+    else
+      { :msg, Enum.reverse(message_buffer) }
+    end
+  end
+
+  defp process(message, sock) do
+    Logger.info("Assembled message by Shell process: #{inspect message}")
   end
 end
 
