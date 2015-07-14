57a0758441684e7324c80e26829a5ca42b795e19
Piotr Przetacznik <piotr.przetacznik@gmail.com>
2015-07-14 13:10:33 +0200

    Parse content in message of shell socket

--- lib/ielixir/heartbeat.ex
+++ lib/ielixir/heartbeat.ex
@@ -8,21 +8,21 @@ defmodule IElixir.Heartbeat do
 
   def init(opts) do
     sock = IElixir.Utils.make_socket(opts, "hb", :rep)
-    { :ok, id } = :erlzmq.getsockopt(sock, :identity)
-    { :ok, { sock, id } }
+    {:ok, id} = :erlzmq.getsockopt(sock, :identity)
+    {:ok, {sock, id}}
   end
 
-  def terminate(_reason, { sock, _ }) do
+  def terminate(_reason, {sock, _ }) do
     :erlzmq.close(sock)
   end
 
-  def handle_info({ :zmq, _, data, [] }, state = { sock, _id }) do
+  def handle_info({:zmq, _, data, []}, state = {sock, _id}) do
     Logger.info("Heartbeat ping received")
     :erlzmq.send(sock, data)
-    { :noreply, state }
+    {:noreply, state}
   end
   def handle_info(msg, state) do
     Logger.warn("Got unexpected message on hb process: #{inspect msg}")
-    { :noreply, state}
+    {:noreply, state}
   end
 end
--- lib/ielixir/shell.ex
+++ lib/ielixir/shell.ex
@@ -8,36 +8,44 @@ defmodule IElixir.Shell do
 
   def init(opts) do
     sock = IElixir.Utils.make_socket(opts, "shell", :router)
-    { :ok, { sock, [] } }
+    {:ok, {sock, []}}
   end
 
-  def terminate(_reason, { sock, _ }) do
+  def terminate(_reason, {sock, _}) do
     :erlzmq.close(sock)
   end
 
-  def handle_info({ :zmq, _, msg, flags }, state = { sock, message_buffer }) do
-    case assemble_message(msg, flags, message_buffer) do
-      { :buffer, buffer } ->
-        { :noreply, { sock, buffer } }
-      { :msg, message } ->
+  def handle_info({:zmq, _, message, flags}, {sock, message_buffer}) do
+    case assemble_message(message, flags, message_buffer) do
+      {:buffer, buffer} ->
+        { :noreply, {sock, buffer}}
+      {:msg, message} ->
         process(message, sock)
-        { :noreply, { sock, [] } }
+        {:noreply, {sock, []}}
     end
   end
-  def handle_info(msg, state) do
-    Logger.warn("Got unexpected message on shell process: #{inspect msg}")
-    { :noreply, state}
+  def handle_info(message, state) do
+    Logger.warn("Got unexpected message on shell process: #{inspect message}")
+    {:noreply, state}
   end
 
   defp assemble_message(message, flags, message_buffer) do
-    message_buffer = [message | message_buffer ]
+    message_buffer = [message | message_buffer]
     if :rcvmore in flags do
-      { :buffer, message_buffer }
+      {:buffer, message_buffer}
     else
-      { :msg, Enum.reverse(message_buffer) }
+      {:msg, List.update_at(Enum.reverse(message_buffer), 3, &(Poison.Parser.parse!(&1)))}
     end
   end
 
+  defp process(message = [_, _, _, %{"msg_type" => msg_type}, _, _, _], sock) do
+    case msg_type do
+      "kernel_info_request" ->
+        Logger.info("Received kernel_info_request")
+      _ ->
+        Logger.info("Received other request: #{inspect msg_type}")
+    end
+  end
   defp process(message, sock) do
     Logger.info("Assembled message by Shell process: #{inspect message}")
   end
