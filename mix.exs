075b760662631b35d0d4d229a601aef44d0c7e60
Piotr Przetacznik <piotr.przetacznik@gmail.com>
2015-08-23 23:22:49 +0200

    Add description in mix.exs file

--- mix.exs
+++ mix.exs
@@ -1,15 +1,21 @@
 defmodule IElixir.Mixfile do
   use Mix.Project
 
+  @version "0.9.0-dev"
+
   def project do
     [app: :ielixir,
-     version: "0.9.0-dev",
+     version: @version,
      source_url: "https://github.com/pprzetacznik/IElixir",
      name: "IElixir",
      elixir: "~> 1.1-dev",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     description: """
+     Jupyter's kernel for Elixir programming language
+     """
+   ]
   end
 
   def application do
