e128cee7f0b76be27f1d18df8be99e68332f15c6
Dmitry Rubinstein <rubinsteindb@gmail.com>
2019-08-14 21:57:09 +0300

    Adding comments

--- lib/ielixir.ex
+++ lib/ielixir.ex
@@ -12,6 +12,7 @@ defmodule IElixir do
                 |> Utils.parse_connection_file
     {:ok, ctx} = :erlzmq.context()
     on_start = IElixir.Supervisor.start_link([conn_info: conn_info, ctx: ctx])
+    # The current working directory is switched after supervision tree has started to properly resolve path to SQLite and config files.
     File.cd!(Application.get_env(:ielixir, :working_directory, File.cwd!))
     on_start
   end
