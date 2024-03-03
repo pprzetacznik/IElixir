7d5f0f0a8a87d7d1bc57dcbf129c248018115cfb
Piotr Przetacznik <piotr.przetacznik@gmail.com>
2024-03-03 20:51:24 +0100

    chore: Refactor install script

--- install_script.sh
+++ install_script.sh
@@ -1,29 +1,30 @@
 #!/bin/sh
 
-if [ -z "$MIX_ENV" ]
-then
-  export MIX_ENV=prod
-fi
+function install_ielixir () {
+  export MIX_ENV=${MIX_ENV:-prod}
 
-# Provide setup according to kernel-spec
-# https://jupyter-client.readthedocs.io/en/latest/kernels.html#kernel-specs
+  # Provide setup according to kernel-spec
+  # https://jupyter-client.readthedocs.io/en/latest/kernels.html#kernel-specs
 
-KERNEL_SPEC="./resources/ielixir"
-NAME="ielixir"
+  KERNEL_SPEC="./resources/ielixir"
+  NAME="ielixir"
+  WORKSPACE=$(cd `dirname "$0"` && pwd)
+  START_SCRIPT_PATH=$WORKSPACE/start_script.sh
 
-START_SCRIPT_PATH=$(cd `dirname "$0"` && pwd)/start_script.sh
+  # help links and language info is provided on connection
+  # by the client
+  CONTENT='{
+     "argv": ["'${START_SCRIPT_PATH}'", "{connection_file}"],
+     "display_name": "Elixir",
+     "language": "Elixir"
+  }'
+  echo $CONTENT | python -m json.tool > $KERNEL_SPEC/kernel.json
 
-# help links and language info is provided on connection
-# by the client
-CONTENT='{
-   "argv": ["'${START_SCRIPT_PATH}'", "{connection_file}"],
-   "display_name": "Elixir",
-   "language": "Elixir"
-}'
-echo $CONTENT | python -m json.tool > $KERNEL_SPEC/kernel.json
+  # for global install remove the --user flag
+  jupyter kernelspec install --user --replace --name=$NAME $KERNEL_SPEC
+  ELIXIR_ERL_OPTIONS="-smp enable" mix ecto.migrate -r IElixir.Repo
 
-# for global install remove the --user flag
-jupyter kernelspec install --user --replace --name=$NAME $KERNEL_SPEC
-ELIXIR_ERL_OPTIONS="-smp enable" mix ecto.migrate -r IElixir.Repo
+  echo "* Installation completed"
+}
 
-echo "* Installation completed"
+install_ielixir
